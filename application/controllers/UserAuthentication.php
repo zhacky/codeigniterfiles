59a66c8cfee61d9fb497cd6c838108f8667ad35f
Zhack Ariya <zhack.alawi@intellis.com.au>
2018-04-07 08:32:00 +0800

    initial files

---
+++ application/controllers/UserAuthentication.php
@@ -0,0 +1,13 @@
+<?php
+namespace zgallery
+
+{
+    session_start();
+    class UserAuthentication extends CI_Controller
+    {
+        public function index()
+        {
+            return;
+        }
+    }
+}
